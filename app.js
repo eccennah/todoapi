f4e7f2adff60f5da1f2029cbed2769616c099c0f
Susannah <adebola.tunmife@gmail.com>
2025-06-19 18:20:51 +0100

    update

--- app.js
+++ app.js
@@ -20,6 +20,22 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/dashboard", auth.verifyToken, async (req, res) => {
+  try {
+    const user_id = req.user_id;
+    const user = req.user;
+
+    const tasks = await taskModel.find({ user_id: user_id });
+
+    // const users = await userModel.find({req.body.first_name })
+    // console.log(articles)
+    res
+      .status(200)
+      .render("dashboard", { user_id, user,  articles, date: new Date() });
+  } catch (err) {
+    return res.json(err);
+  }
+});
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
